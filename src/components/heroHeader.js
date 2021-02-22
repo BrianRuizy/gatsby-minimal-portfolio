181a17d57acf3b54a7c1bed1e01a957abd37868a
brian <brianruiz123@yahoo.com>
2021-02-22 01:15:56 -0600

    hero update

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { StaticQuery, graphql, Link } from "gatsby"
+
 export default () => (
   <StaticQuery
     query={graphql`
@@ -17,10 +18,12 @@ export default () => (
     render={data => (
       <div className="hero-header">
         <div className="headline">{data.site.siteMetadata.home.title}</div>
-        <div 
-          className="primary-content" 
-          dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
-        />
+        <div className="primary-content">
+          <p>
+          I'm a Software Developer, specializing in Python full stack development.
+          I'm a data nerd, know cloud computing, enjoy front-end design, & proponent of open-source.
+          </p>
+        </div>
         <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
       </div>
     )}
