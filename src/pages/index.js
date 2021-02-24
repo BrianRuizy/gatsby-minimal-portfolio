4018852821bde7b20ef53838c7a909dca528f6be
brian <brianruiz123@yahoo.com>
2021-02-24 02:53:25 -0600

    cleaning project

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const IndexPage = ({
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
       <HeroHeader/>
-      <h2>Blog Posts &darr;</h2>
+      <h2>Projects &darr;</h2>
       <div className="grids">
         {Posts}
       </div>
