0bcabd34a718201f93e5ff33bd7fc975b4ab8d36
brian <brianruiz123@yahoo.com>
2021-02-24 02:31:44 -0600

    about page added

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,6 +4,7 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
+    <Link to="/about">About</Link>
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
