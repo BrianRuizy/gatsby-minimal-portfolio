671c06939e4be9174f751cb1db24c8f1c2ce1a90
brian <brianruiz123@yahoo.com>
2021-02-27 23:58:26 -0600

    added bootstrap navbar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,12 +1,23 @@
 import React from "react"
 import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
+import Navbar from 'react-bootstrap/Navbar'
 
 export default (props) => (
-  <nav className="navigation"> 
-    <Link to="/about">About</Link>
-    <Link to="/contact">Contact</Link>
-    <ThemeChanger/>
-  </nav>
-  
+
+  <Navbar >
+    <Navbar.Brand href="#home"> 
+      <div className="site-title">
+        <Link to="/">BR</Link>
+      </div>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.Collapse className="justify-content-end">
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <ThemeChanger/>
+    </Navbar.Collapse>
+  </Navbar>
+
+
 )
