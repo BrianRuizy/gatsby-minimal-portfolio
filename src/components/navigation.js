aa1edc0d09e25e01b8c6cb804bdd815bac5d2508
brian <brianruiz123@yahoo.com>
2021-03-03 17:50:24 -0600

    fix nav link

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,14 +6,13 @@ import Navbar from 'react-bootstrap/Navbar'
 export default (props) => (
 
   <Navbar className="py-3" sticky="top" >
-    <Navbar.Brand href="#home">Brian Ruiz</Navbar.Brand>
-    <Navbar.Toggle />
+    <Navbar.Brand >
+      <Link to="/">Brian Ruiz</Link>
+    </Navbar.Brand>
     <Navbar.Collapse className="justify-content-end">
       <Link to="/about">About</Link>
       <Link to="/contact">Contact</Link>
       <ThemeChanger/>
     </Navbar.Collapse>
   </Navbar>
-
-
 )
