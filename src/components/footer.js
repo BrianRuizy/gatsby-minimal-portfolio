9ed011f8063f4aa7501091a9f300b63e4001f707
brian <brianruiz123@yahoo.com>
2021-03-03 15:59:17 -0600

    made footer component

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default (props) => (
+  <footer className="site-footer">
+    <p>
+      Made with <span role="img" aria-label="love">❤️</span> and
+      <span role="img" aria-label="love"> ☕</span><br></br>
+      &copy; {new Date().getFullYear()} Brian Ruiz 
+    </p>
+  </footer>
+)
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import Navigation from "../components/navigation"
+import Footer from "../components/footer"
+
 import 'prismjs/themes/prism-tomorrow.css';
 
 export default ({ children }) => {
@@ -19,13 +21,7 @@ export default ({ children }) => {
     <div className="site-wrapper">
       <Navigation />
       {children}
-      <footer className="site-footer">
-        <p>
-          Made with <span role="img" aria-label="love">❤️</span> and
-          <span role="img" aria-label="love"> ☕</span><br></br>
-          &copy; {new Date().getFullYear()} Brian Ruiz 
-        </p>
-      </footer>
+      <Footer />
     </div>
   )
 }
