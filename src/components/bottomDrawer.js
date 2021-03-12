02ea7286931a4cde159e503f4532ab1865b1011c
Brian Ruiz <brianruiz123@yahoo.com>
2021-03-12 11:26:57 -0600

    typo

--- src/components/bottomDrawer.js
+++ src/components/bottomDrawer.js
@@ -26,7 +26,7 @@ export default function App() {
         <Row>
           <Col lg={6} md={8} sm={10} xs={11} className="contact-form">
             <form name="contact" method="POST" data-netlify="true">
-              <input type="hidden" name="form-name" value="contact">
+              <input type="hidden" name="form-name" value="contact"/>
               <label htmlFor="defaultFormContactNameEx" >
                 Your name
               </label>
