6573d5a5e63d81b334b9409034fc42e53139e916
brian <brianruiz123@yahoo.com>
2021-03-14 18:38:11 -0500

    testing contact fom

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,7 @@ import React from "react"
 import Helmet from "react-helmet"
 import { graphql } from 'gatsby'
 import Layout from "../components/layout"
+import { Container } from "react-bootstrap"
 
 const ContactPage = ({
   data: {
@@ -14,6 +15,32 @@ const ContactPage = ({
         <title>Contact — {site.siteMetadata.title}</title>
         <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
       </Helmet>
+      <Container>
+      <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact"/>
+        <label htmlFor="defaultFormContactNameEx" >
+          Your name
+        </label>
+        <input type="text" id="defaultFormContactNameEx" className="form-control" />
+        <br />
+        <label htmlFor="defaultFormContactEmailEx" >
+          Your email
+        </label>
+        <input type="email" id="defaultFormContactEmailEx" className="form-control" />
+        <br />
+        <label htmlFor="defaultFormContactSubjectEx" >
+          Subject
+        </label>
+        <input type="text" id="defaultFormContactSubjectEx" className="form-control" />
+        <br />
+        <label htmlFor="defaultFormContactMessageEx" >
+          Your message
+        </label>
+        <textarea type="text" id="defaultFormContactMessageEx" className="form-control" rows="3" />
+        <Button className="mt-4" type="submit" size="md">Send</Button>
+
+        </form>
+      </Container>
     </Layout>
   )
 }
