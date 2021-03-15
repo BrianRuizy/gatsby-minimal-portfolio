5707ab2d0f124018f2dc384fe21a92baae89cf0a
Brian Ruiz <brianruiz123@yahoo.com>
2021-03-15 02:31:35 -0500

    remove form from contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -19,30 +19,7 @@ const ContactPage = ({
         <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
       </Helmet>
       <Container>
-      <form name="contact" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact"/>
-        <label htmlFor="defaultFormContactNameEx" >
-          Your name
-        </label>
-        <input type="text" id="defaultFormContactNameEx" className="form-control" />
-        <br />
-        <label htmlFor="defaultFormContactEmailEx" >
-          Your email
-        </label>
-        <input type="email" id="defaultFormContactEmailEx" className="form-control" />
-        <br />
-        <label htmlFor="defaultFormContactSubjectEx" >
-          Subject
-        </label>
-        <input type="text" id="defaultFormContactSubjectEx" className="form-control" />
-        <br />
-        <label htmlFor="defaultFormContactMessageEx" >
-          Your message
-        </label>
-        <textarea type="text" id="defaultFormContactMessageEx" className="form-control" rows="3" />
-        <Button className="mt-4" type="submit" size="md">Send</Button>
-
-        </form>
+     
       </Container>
     </Layout>
   )
