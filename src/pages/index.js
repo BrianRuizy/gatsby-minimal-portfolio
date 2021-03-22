ffc6eb0d65d6e82f6aae25cc0cf02733ea774c15
brian <brianruiz123@yahoo.com>
2021-03-22 11:55:28 -0500

    new open graph image

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,9 +16,9 @@ const IndexPage = ({
       <Helmet>
         <title>{site.siteMetadata.title}</title>
         <meta name="description" content={site.siteMetadata.description} />
-        <meta property="og:image" content="https://user-images.githubusercontent.com/23439187/111946587-becacd00-8aa9-11eb-96ee-a5399fa2658a.png"/>
-        <meta property="og:image:width" content="1600" />
-        <meta property="og:image:width" content="622" />
+        <meta property="og:image" content="https://user-images.githubusercontent.com/23439187/112022247-1ac04080-8b00-11eb-8f0c-2954ac58f221.png"/>
+        <meta property="og:image:width" content="2139" />
+        <meta property="og:image:width" content="1126" />
       </Helmet>
       <Hero/>
       <Featured />
