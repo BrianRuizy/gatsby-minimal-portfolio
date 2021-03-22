857849abd0ee5061640aa5316fcd4f41033398d9
brian <brianruiz123@yahoo.com>
2021-03-21 23:37:27 -0500

    changed footer and container paddings

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,8 @@ import React from "react"
 export default (props) => (
   <footer className="site-footer py-5">
     <p>
-      &copy; {new Date().getFullYear()} Brian Ruiz 
+      &copy; {new Date().getFullYear()} 
+      <a href="https://github.com/BrianRuizy/portfolio-website/" target="_blank"> Brian Ruiz</a>.
     </p>
   </footer>
 )
