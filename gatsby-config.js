83ba1d9a436c465e0c8cd28d9688c7613ee71690
brian <brianruiz123@yahoo.com>
2021-03-23 00:32:55 -0500

    added google gtag plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,23 +48,42 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "UA-164743872-1",
-        head: true,
-      }
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Brian Ruiz Portfolio Wesbite`,
+        name: `Brian Ruiz Portfolio`,
         short_name: `brianruizy`,
         start_url: `/`,
         background_color: `#fafafa`,
         theme_color: `#381696`,
         display: `standalone`,
         icon: "src/images/icon.png",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-106940902-3", // Google Analytics / GA
+          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     `gatsby-plugin-sass`, 
