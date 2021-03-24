6ce05ec49d98a7a120aca00586651819bb3aacdf
brian <brianruiz123@yahoo.com>
2021-03-24 03:50:53 -0500

    changed footer, hero h1 font

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,13 @@ import React from "react"
 
 export default (props) => (
   <footer className="site-footer py-5">
-    <p>
-      &copy; {new Date().getFullYear()} 
-      <a href="https://github.com/BrianRuizy/gatsby-minimal-portfolio/" target="_blank" rel="noopener noreferrer"> Gatsby Minimal Portfolio</a>.
+    <a href="https://app.netlify.com/sites/brianruizy/deploys" target="_blank" rel="noopener noreferrer">
+      <img src="https://api.netlify.com/api/v1/badges/72511ec5-84cd-416c-81d8-b16489c1b235/deploy-status" alt="Netlify Status" /> 
+    </a>
+    <p className="mt-1">
+      &copy;{ new Date().getFullYear() } 
+      <a href="https://github.com/BrianRuizy/gatsby-minimal-portfolio/" target="_blank" rel="noopener noreferrer"> Portfolio</a>,
+      by Brian Ruiz.
     </p>
   </footer>
 )
