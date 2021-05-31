3ad62b6efe6b1ba9b07a6f88666eb4043f9abbc1
brian <brianruiz123@yahoo.com>
2021-05-31 17:57:38 -0500

    added material-kai project

---
+++ _data/projects/materialkai.md
@@ -0,0 +1,10 @@
+---
+template: projects
+name: Material-Kai
+github: 'https://github.com/BrianRuizy/material-kai'
+demo: 'https://marketplace.visualstudio.com/items?itemName=brianruizy.material-kai'
+tags: 
+    - VS Code
+    - 
+---
+An elegant Hybrid of Material and Monokai. Diligently tweaked to reduce distractions and increase your productivity
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -7,7 +7,6 @@ import { makeStyles } from "@material-ui/core/styles"
 import Typography from '@material-ui/core/Typography'
 import Container from "@material-ui/core/Container"
 import Button from "@material-ui/core/Button"
-
 import GetAppOutlinedIcon from '@material-ui/icons/GetAppOutlined'
 
 
@@ -25,7 +24,7 @@ else if (hour === 23 || hour < 5)
 
 const useStyles = makeStyles((theme) => ({
   button: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(1.5),
   },
 }));
 
